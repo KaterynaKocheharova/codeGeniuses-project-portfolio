eaf56be1cee0ba052869d493164912ecf56548e8
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-12 10:29:42 +0300

    js-markup-done

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,31 +1,35 @@
-import { getReviews } from "./API";
+import { getReviews } from './API';
 
-  async function onCreateReview() {
-    const reviews = await getReviews();
-    console.log(reviews)
-    // const markup = userTemplate(newUser);
-    // refs.userListElem.insertAdjacentHTML('afterbegin', markup);
-  }
-  
+const reviewsList = document.querySelector('.reviews-list');
 
-  onCreateReview()
-  // ============================================================
-  
-//   function userTemplate({ id, name, img, email, phone }) {
-//     return `<li class="card user-item" data-id="${id}">
-//     <img
-//       src="https://source.unsplash.com/720x1280/?random=${id}&girl,portret,celebrity"
-//       alt="#"
-//       class="user-avatar"
-//     />
-//     <h3 class="user-title">${name}</h3>
-//     <p>Phone: ${email}</p>
-//     <p>Email: ${phone}</p>
-//     <button class="btn button">DELETE</button>
-//   </li>`;
-//   }
-  
-//   function usersTemplate(arr) {
-//     return arr.map(userTemplate).join('\n\n\n\n');
-//   }
-  
+async function onCreateReview() {
+  const reviews = await getReviews();
+  const reviewsMarkup = reviewsTemplate(reviews);
+  reviewsList.insertAdjacentHTML('afterbegin', reviewsMarkup);
+}
+
+onCreateReview();
+
+function reviewsTemplate(arr) {
+  return arr.map(reviewTemplate).join('');
+}
+
+function reviewTemplate({ _id, author, avatar_url, review }) {
+  return `
+      <li class="reviews-item" id="${_id}">
+        <img
+          class="reviews-image"
+          src="${avatar_url}"
+          alt="reviewer's avatar"
+          width="48"
+          height="48"
+        />
+        <h3 class="section-subtitle reviews-subtitle">${author}</h3>
+        <p class="section-text reviews-text">
+          Work with was extraordinary! He turned out to be a very competent and
+          responsible specialist. The projects were completed on time and the
+          result exceeded my expectations
+        </p>
+      </li>
+  `;
+}
