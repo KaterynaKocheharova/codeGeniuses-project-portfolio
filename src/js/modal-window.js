2eff3ba59a525bd4f589dc11f8fe63ff1121c464
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-04-14 01:09:57 +0300

    together

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,27 +1,36 @@
 const modalWindow = document.querySelector('.modal-window');
 const backdrop = document.querySelector('.backdrop');
-
 const closeBtn = document.querySelector('.close-btn');
+const titleElement = document.querySelector('.modal-title');
+const textElement = document.querySelector('.modal-paragraf');
 
-closeBtn.addEventListener('click', () => {
-  modalWindow.classList.add('hidden');
-  backdrop.classList.add('hidden');
-});
-
-window.addEventListener('keydown', event => {
-  if (event.key === 'Escape') {
-    closeModal();
+class ModalWindow {
+  open() {
+    modalWindow.classList.remove('hidden');
+    backdrop.classList.remove('hidden');
   }
-});
 
-function openModal() {
-  modalWindow.classList.remove('hidden');
-  backdrop.classList.remove('hidden');
+  close() {
+    modalWindow.classList.add('hidden');
+    backdrop.classList.add('hidden');
+  }
+
+  render(title, text) {
+    titleElement.innerHTML = title;
+    textElement.innerHTML = text;
+  }
+
+  bindEvents() {
+    closeBtn.addEventListener('click', () => {
+      this.close();
+    });
+
+    window.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        this.close();
+      }
+    });
+  }
 }
 
-function closeModal() {
-  modalWindow.classList.add('hidden');
-  backdrop.classList.add('hidden');
-}
-
-export { openModal, closeModal };
+export default new ModalWindow();
