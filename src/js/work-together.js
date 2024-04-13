8d66dcf44fbc28aae784db1036a4c0bfa8c04cfe
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-04-13 22:42:15 +0300

    together

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,19 +1,17 @@
-import { apiPost, apiGet } from './api';
-import { openModal, closeModal } from './modal-window';
-
+import { sendInfo } from './API';
+import { openModal } from './modal-window';
 const form = document.querySelector('.form-section');
-
 form.addEventListener('submit', handleSubmitForm);
-
 async function handleSubmitForm(event) {
   event.preventDefault();
-
-  const { email, number } = event.target.elements;
-  console.log(email.value);
-  console.log(number.value);
+  const email = event.currentTarget.elements.hemail;
+  const comment = event.currentTarget.elements.comment;
 
   try {
-    const response = await apiPost(email.value, number.value);
+    const response = await sendInfo({
+      email: email.value,
+      comment: comment.value,
+    });
     console.log(response);
     if (response) {
       openModal();
@@ -21,6 +19,6 @@ async function handleSubmitForm(event) {
     }
   } catch (error) {
     console.error('Error submitting request:', error);
-    form.reset();
   }
+  form.reset();
 }
