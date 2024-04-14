ab7f13fec0c2ad4361341a808eb9a2ea13245e8f
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-04-14 15:05:37 +0300

    +

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,24 +1,62 @@
-import { sendInfo } from './API';
-import { openModal } from './modal-window';
+import { apiPost } from './api';
+import modalWindow from './modal-window';
 const form = document.querySelector('.form-section');
-form.addEventListener('submit', handleSubmitForm);
-async function handleSubmitForm(event) {
+const emailInput = document.querySelector('input[name="email"]');
+const commentInput = document.querySelector('input[name="comment"]');
+const success = document.querySelector('.success-text');
+const error = document.querySelector('.error-text');
+function showSuccess() {
+  success.classList.remove('visually-hidden');
+  error.classList.add('visually-hidden');
+}
+function showError() {
+  success.classList.add('visually-hidden');
+  error.classList.remove('visually-hidden');
+}
+function hideMessages() {
+  success.classList.add('visually-hidden');
+  error.classList.add('visually-hidden');
+}
+commentInput.addEventListener('click', () => {
+  if (emailInput.validity.valid) {
+    showSuccess();
+  } else {
+    showError();
+  }
+});
+function clearInputClassesAndText() {
+  emailInput.classList.remove('success');
+  emailInput.classList.remove('error');
+  hideMessages();
+}
+emailInput.addEventListener('click', () => {
+  hideMessages();
+});
+emailInput.addEventListener('input', function () {
+  if (this.validity.valid) {
+    this.classList.remove('error');
+    this.classList.add('success');
+  } else {
+    this.classList.remove('success');
+    this.classList.add('error');
+  }
+});
+form.addEventListener('submit', async event => {
   event.preventDefault();
-  const email = event.currentTarget.elements.hemail;
-  const comment = event.currentTarget.elements.comment;
-
+  const { email, comment } = event.target.elements;
   try {
-    const response = await sendInfo({
-      email: email.value,
-      comment: comment.value,
-    });
+    const response = await apiPost(email.value, comment.value);
     console.log(response);
     if (response) {
-      openModal();
+      modalWindow.render(response.title, response.message);
+      modalWindow.open();
+      clearInputClassesAndText();
       form.reset();
     }
   } catch (error) {
     console.error('Error submitting request:', error);
+    clearInputClassesAndText();
+    form.reset();
   }
-  form.reset();
-}
+});
+modalWindow.bindEvents();
