ade05dd6377cf26d3c7c4960060a8cb815c163a3
Maksym <106184045+Makzumm@users.noreply.github.com>
2024-04-15 00:31:24 +0200

    Fix

--- src/js/projects.js
+++ src/js/projects.js
@@ -5,45 +5,45 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 let params = {
-  modules: [Navigation, Mousewheel, Keyboard],
+    modules: [Navigation, Mousewheel, Keyboard],
 
-  slidesPerView: 'auto',
-  spaceBetween: 100,
-  loop: false,
-  centeredSlides: true,
-  direction: 'horizontal',
-  mousewheel: true,
-  keyboard: true,
+    slidesPerView: 'auto',
+    spaceBetween: 100,
+    loop: false,
+    centeredSlides: true,
+    direction: 'horizontal',
+    mousewheel: true,
+    keyboard: true,
 
-  navigation: {
-    nextEl: '.custom-swiper-btn-next',
-    prevEl: '.custom-swiper-btn-prev',
-  },
-  on: {
-    init: function () {
-      this.navigation.prevEl.setAttribute(
-        'data-swiper-button-disabled',
-        'true'
-      );
+    navigation: {
+        nextEl: '.custom-swiper-btn-next',
+        prevEl: '.custom-swiper-btn-prev',
     },
-    slideChange: function () {
-      if (this.activeIndex === 0) {
-        this.navigation.prevEl.setAttribute(
-          'data-swiper-button-disabled',
-          'true'
-        );
-      } else {
-        this.navigation.prevEl.removeAttribute('data-swiper-button-disabled');
-      }
-      if (this.activeIndex === this.slides.length - 1) {
-        this.navigation.nextEl.setAttribute(
-          'data-swiper-button-disabled',
-          'true'
-        );
-      } else {
-        this.navigation.nextEl.removeAttribute('data-swiper-button-disabled');
-      }
+    on: {
+        init: function () {
+            this.navigation.prevEl.setAttribute(
+                'data-swiper-button-disabled',
+                'true'
+            );
+        },
+        slideChange: function () {
+            if (this.activeIndex === 0) {
+                this.navigation.prevEl.setAttribute(
+                    'data-swiper-button-disabled',
+                    'true'
+                );
+            } else {
+                this.navigation.prevEl.removeAttribute('data-swiper-button-disabled');
+            }
+            if (this.activeIndex === this.slides.length - 1) {
+                this.navigation.nextEl.setAttribute(
+                    'data-swiper-button-disabled',
+                    'true'
+                );
+            } else {
+                this.navigation.nextEl.removeAttribute('data-swiper-button-disabled');
+            }
+        },
     },
-  },
 };
-const swiper = new Swiper('.swiper-container', params);
+const swiperProjects = new Swiper('.swiper-container', params);
