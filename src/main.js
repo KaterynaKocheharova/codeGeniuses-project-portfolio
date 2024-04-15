f900b64bc1811c77130f9a57f24f2dc5414a652c
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-16 00:08:49 +0300

    clean-work-together-code-1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import "./js/header"
-import "./js/hero"
-import "./js/about-me"
-import "./js/benefits"
-import "./js/projects"
-import "./js/FAQ"
-import "./js/covers"
-import "./js/reviews"
-import "./js/work-together"
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/projects';
+import './js/FAQ';
+import './js/covers';
+import './js/reviews';
+import './js/work-together';
