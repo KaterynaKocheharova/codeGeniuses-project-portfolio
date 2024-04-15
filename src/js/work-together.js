4dff9093fb8e1ee1e39a5de6b7cbfa10b068e7ca
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-15 11:56:41 +0300

    add catching error

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -13,10 +13,20 @@ async function handleFormSubmit(event) {
     comment: event.currentTarget.elements.comment.value,
   };
 
-  const data = await sendInfo(user);
-  console.log(data);
-  if (data) {
+  try {
+    const data = await sendInfo(user);
     openModal();
+    form.reset();
+  } catch (error) {
+    iziToast.show({
+      title: `@{error}`,
+      titleColor: 'rgba(250, 250, 250, 0.6)',
+      titleLineHeight: '1.2',
+      message:
+        'Sorry. There is a problem with your request. Edit your message please',
+      messageColor: 'rgba(250, 250, 250, 0.6)',
+      backgroundColor: 'rgb(237, 59, 68)',
+    });
   }
 }
 
