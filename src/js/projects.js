ba42ae757159707cab90180c8213b4e6e59ee2f6
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-17 23:59:36 +0300

    remove not used js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, Mousewheel, Keyboard } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
