8499592bd0b8bbf9a938ef7bac3fa17838cb0e9e
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-17 09:02:06 +0300

    clean-header-css

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,16 +1,18 @@
+const heroTitle = document.querySelector('.hero-title');
 
-const heroTitle = document.getElementById("heroTitle");
+document.addEventListener('DOMContentLoaded', animateOnScroll);
+window.addEventListener('scroll', animateOnScroll);
 
 function animateOnScroll() {
-    const rect = heroTitle.getBoundingClientRect();
-    const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  const rect = heroTitle.getBoundingClientRect();
+  const viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
 
-    if (rect.top >= 0 && rect.bottom <= viewHeight) {
-        heroTitle.classList.add("show"); 
-    } else {
-        heroTitle.classList.remove("show");
-    }
+  if (rect.top >= 0 && rect.bottom <= viewHeight) {
+    heroTitle.classList.add('show');
+  } else {
+    heroTitle.classList.remove('show');
+  }
 }
-
-document.addEventListener("DOMContentLoaded", animateOnScroll);
-window.addEventListener("scroll", animateOnScroll);
