3adbb3444a4e569b47dc0a2e36c77c0b9f53e048
Kateryna Kocheharova <kot41901@gmail.com>
2024-04-26 13:29:05 +0300

    clean js code-1

--- src/js/header.js
+++ src/js/header.js
@@ -1,60 +1,50 @@
+// ===================== MOBILE MENU VARIABLES
 const headerBurger = document.querySelector('.header-burger');
 const modalMenu = document.querySelector('.modal-menu');
-const closeBtn = document.querySelector('.close-btn-menu');
-const dropdownMenu = document.getElementById('menu');
+const closeMenuBtn = document.querySelector('.close-btn-menu');
 const mobMenuLink = document.querySelectorAll('.modal-mob-menu-link');
-const mobMenuBtn = document.querySelector('.mob-menu-button-order');
+const mobMenuOrderBtn = document.querySelector('.mob-menu-button-order');
+
+// ====================== DROPDOWN VARIABLES
+const dropdownMenu = document.getElementById('menu');
 const dropdownBoxes = document.querySelectorAll('.dropdown-box');
 const dropdownItem = document.querySelectorAll('.dropdown-item');
 const dropdownBtn = document.querySelector('.dropdown-btn-menu');
-const body = document.querySelector('body');
 
 // ======================= OPEN MODAL
-
-headerBurger.addEventListener('click', openModal);
-
 function openModal(e) {
   e.preventDefault();
   modalMenu.classList.add('is-open');
-  body.classList.add('modal-is-open');
+  document.body.classList.add('modal-is-open');
 }
+
+headerBurger.addEventListener('click', openModal);
 
 // ======================= CLOSE MODAL
+function closeModal() {
+  modalMenu.classList.remove('is-open');
+  document.body.classList.remove('modal-is-open');
+}
 
-closeBtn.addEventListener('click', closeModal);
-
+closeMenuBtn.addEventListener('click', closeModal);
 mobMenuLink.forEach(elem => {
-  elem.addEventListener('click', linksCloseModal);
+  elem.addEventListener('click', closeModal);
 });
-
-mobMenuBtn.addEventListener('click', linksCloseModal);
-
-function closeModal(e) {
-  e.preventDefault();
-  modalMenu.classList.remove('is-open');
-  body.classList.remove('modal-is-open');
-}
-
-function linksCloseModal(e) {
-  modalMenu.classList.remove('is-open');
-  body.classList.remove('modal-is-open');
-}
+mobMenuOrderBtn.addEventListener('click', closeModal);
 
 // ====================== DROPDOWN
-
-dropdownBtn.addEventListener('click', e => {
-  e.preventDefault();
+function onDropdownBtnClick() {
   dropdownMenu.classList.toggle('show');
-});
-
-dropdownBtn.addEventListener('click', () => {
   dropdownBoxes.forEach(box => {
     box.classList.add('pointer-auto');
   });
-});
+  dropdownItem.forEach(item =>
+    item.addEventListener('click', onDropdownItemClick)
+  );
+}
 
-dropdownItem.forEach(item =>
-  item.addEventListener('click', () => {
-    dropdownMenu.classList.remove('show', 'pointer-auto');
-  })
-);
+function onDropdownItemClick() {
+  dropdownMenu.classList.remove('show', 'pointer-auto');
+}
+
+dropdownBtn.addEventListener('click', onDropdownBtnClick);
